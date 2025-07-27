2a1f1f9e70b60e4f4d37add8c5554d63a5284195
IlMelons <melonss2002@gmail.com>
2025-07-27 13:51:54 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,30 @@
+fx_version "cerulean"
+game "gta5"
+lua54 "yes"
+
+name "mnr_police"
+description "Police Job"
+author "IlMelons"
+version "1.0.0"
+repository "https://www.github.com/Monarch-Development/mnr_police"
+
+ox_lib "locale"
+
+shared_scripts {
+	"@ox_lib/init.lua",
+	"config/*.lua",
+}
+
+client_scripts {
+	"bridge/client/**/*.lua",
+	"client/*.lua",
+}
+
+server_scripts {
+	"bridge/server/**/*.lua",
+	"server/*.lua",
+}
+
+files {
+	"locales/*.json",
+}
